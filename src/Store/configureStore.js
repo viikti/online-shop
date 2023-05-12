a6007c4c327584dc7be870002ff00b3fd87896fb
My Name <viikti@mail.ru>
2023-05-12 14:18:45 +0200

    fixed store and rootReducer

--- src/Store/configureStore.js
+++ src/Store/configureStore.js
@@ -1,30 +1,30 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-//
-// import { rootReducer } from "./rootReducer";
-//
-// console.log(rootReducer);
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   whitelist: ["auth"],
-// };
-//
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-//
-// export const store = configureStore({
-//   reducer: persistedReducer,
-// });
-//
-// export const persistor = persistStore(store);
-
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./rootReducer";
 
+console.log(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+// import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import { configureStore } from "@reduxjs/toolkit";
+//
+// import { rootReducer } from "./rootReducer";
+
 // const persistConfig = {
 //   key: "root",
 //   storage,
@@ -32,7 +32,7 @@ import { rootReducer } from "./rootReducer";
 // };
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
-  reducer: rootReducer,
-});
-export const persistor = persistStore(store);
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
+// export const persistor = persistStore(store);
--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -1,20 +1,20 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import signInReducer from "../pages/SignIn/reducers";
 import dataFetchingReducer from "../pages/Pokemons/reducers";
-import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/lib/storage";
 import pokemonDetailsSlice from "../pages/PokemonDetails/reducers";
 import cartSlice from "../pages/Cart/reducers/cartReducer";
 
-import { persistReducer } from "redux-persist";
+// import { persistReducer } from "redux-persist";
 
-const persistConfig = {
-  key: "auth",
-  storage,
-  whitelist: ["auth", "data"],
-};
+// const persistConfig = {
+//   key: "auth",
+//   storage,
+//   whitelist: ["auth", "data"],
+// };
 
 export const rootReducer = combineReducers({
-  auth: persistReducer(persistConfig, signInReducer),
+  auth: signInReducer,
   dataFetching: dataFetchingReducer,
   pokemonDetail: pokemonDetailsSlice,
   cart: cartSlice,
